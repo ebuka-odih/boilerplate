da1cf50e2057587d8975e0ea31fe6a0a40696459
Gnosis <emmagab38@gmail.com>
2024-10-30 22:05:31 +0100

    commit changes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,12 +4,39 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
         return view('admin.index');
+    }
+
+    public function settings()
+    {
+        $user = Auth::user();
+        return view('admin.setting.profile', compact('user'));
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+        $request->validate(
+            ['name' => 'required']
+        );
+
+        $user->name = $request['name'];
+        $user->save();
+        return redirect()->back()->with('success', 'Profile updated successfully');
+    }
+
+    public function activities()
+    {
+        $user = Auth::user();
+        $data = DB::table('sessions')->whereUserId(\auth()->id())->get();
+        return view('admin.setting.activities', compact('data', 'user'));
     }
 
 }
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth', 'verified', 'admin'], 'prefix' => 'admin', 'as' => 'admin.'], function(){
      Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
      Route::get('/settings', [AdminController::class, 'settings'])->name('settings');
+     Route::post('/update/profile', [AdminController::class, 'updateProfile'])->name('updateProfile');
+     Route::get('/activities', [AdminController::class, 'activities'])->name('activities');
      Route::get('/security', [AdminController::class, 'security'])->name('security');
 
      Route::get('users', [UserController::class, 'index'])->name('users.index');
